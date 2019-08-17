1b21b9205ebb513d81f6e3a9030d50155ec9e978
Miguel Ángel Fernandez Carrillo <mfernandez@quantion.es>
2019-08-17 18:05:17 +0200

    Initial Commit

---
+++ calculadora/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  calculadora
+//
+//  Created by Miguel Angel Fernandez Carrillo on 17/08/2019.
+//  Copyright © 2019 Miguel Angel Fernandez Carrillo. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
